5c97a51ca5de5a47077311c003d2d705d8f9f694
lock1205 <mindheal205@gmail.com>
2024-09-30 17:34:50 +0900

    index change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); //express 모듈 불러오기
 const cors = require('cors'); //cors 모듈 불러오기
-const PORT = '8000';
+const PORT = '8080';
 const cookieParser = require('cookie-parser');
 
 const app = express(); //express 모듈을 사용하기 위해 app 변수에 할당한다.
