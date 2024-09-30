d37e6ef5db88983e594875e42f8e3f127fae9f83
lock1205 <mindheal205@gmail.com>
2024-09-30 18:50:54 +0900

    commit

--- index.js
+++ index.js
@@ -35,14 +35,17 @@ app.use(
 // 다른 출처 사이의 통신에서 보안을 강화하고 싶을 때 사용하는 방법으로, 헤더에 인증 정보를 담아 요청을 보낸다.
 // 기본적으로 출처가 다른 경우에는 쿠키나 인증 관련 헤더(헤더에 Authorization 항목이 있는 요청)를 보낼 수 없다. 민감한 정보이기 때문이다. 때문에 그것이 가능하도록 하려면 프론트와 서버 양측 모두 CORS를 설정해야 한다.
 // 프론트엔드의 요청과 서버의 응답 모두 CORS 설정이 필요하다.
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('production back site private!');
 });
 
-/************************************************* */
+app.get('/test', (req, res) => {
+  res.send('test back site private!');
+});
 
-app.use(bodyParser.json());
+/************************************************* */
 
 // app.post('/chat', (req, res) => {
 //   const sendQuestion = req.body.question;
